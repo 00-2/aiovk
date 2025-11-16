1f97f9fd95a2a8eb020bacbc27405316fc1821a5
00-2 <9x02@mail.ru>
2025-11-16 16:04:44 +0000

    add vkBotEventType

--- aiovk/longpoll.py
+++ aiovk/longpoll.py
@@ -279,6 +279,99 @@ class VkEventType(IntEnum):
     #: $disabled_until — выключение оповещений на необходимый срок.
     NOTIFICATION_SETTINGS_UPDATE = 114
 
+class VkBotEventType(IntEnum):
+    """ Перечисление событий, получаемых от longpoll-сервера.
+
+    `Подробнее в документации VK API
+    <https://vk.ru/dev/using_longpoll?f=3.+Структура+событий>`__
+    """
+
+    #: Замена флагов сообщения (FLAGS:=$flags)
+    MESSAGE_FLAGS_REPLACE = 1
+
+    #: Установка флагов сообщения (FLAGS|=$mask)
+    MESSAGE_FLAGS_SET = 2
+
+    #: Сброс флагов сообщения (FLAGS&=~$mask)
+    MESSAGE_FLAGS_RESET = 3
+
+    #: Добавление нового сообщения.
+    MESSAGE_NEW = 'message_new'
+
+    #: Редактирование сообщения.
+    MESSAGE_EDIT = 5
+
+    #: Прочтение всех входящих сообщений в $peer_id,
+    #: пришедших до сообщения с $local_id.
+    READ_ALL_INCOMING_MESSAGES = 6
+
+    #: Прочтение всех исходящих сообщений в $peer_id,
+    #: пришедших до сообщения с $local_id.
+    READ_ALL_OUTGOING_MESSAGES = 7
+
+    #: Друг $user_id стал онлайн. $extra не равен 0, если в mode был передан флаг 64.
+    #: В младшем байте числа extra лежит идентификатор платформы
+    #: (см. :class:`VkPlatform`).
+    #: $timestamp — время последнего действия пользователя $user_id на сайте.
+    USER_ONLINE = 8
+
+    #: Друг $user_id стал оффлайн ($flags равен 0, если пользователь покинул сайт и 1,
+    #: если оффлайн по таймауту). $timestamp — время последнего действия пользователя
+    #: $user_id на сайте.
+    USER_OFFLINE = 9
+
+    #: Сброс флагов диалога $peer_id.
+    #: Соответствует операции (PEER_FLAGS &= ~$flags).
+    #: Только для диалогов сообществ.
+    PEER_FLAGS_RESET = 10
+
+    #: Замена флагов диалога $peer_id.
+    #: Соответствует операции (PEER_FLAGS:= $flags).
+    #: Только для диалогов сообществ.
+    PEER_FLAGS_REPLACE = 11
+
+    #: Установка флагов диалога $peer_id.
+    #: Соответствует операции (PEER_FLAGS|= $flags).
+    #: Только для диалогов сообществ.
+    PEER_FLAGS_SET = 12
+
+    #: Удаление всех сообщений в диалоге $peer_id с идентификаторами вплоть до $local_id.
+    PEER_DELETE_ALL = 13
+
+    #: Восстановление недавно удаленных сообщений в диалоге $peer_id с
+    #: идентификаторами вплоть до $local_id.
+    PEER_RESTORE_ALL = 14
+
+    #: Один из параметров (состав, тема) беседы $chat_id были изменены.
+    #: $self — 1 или 0 (вызваны ли изменения самим пользователем).
+    CHAT_EDIT = 51
+
+    #: Изменение информации чата $peer_id с типом $type_id
+    #: $info — дополнительная информация об изменениях
+    CHAT_UPDATE = 52
+
+    #: Пользователь $user_id набирает текст в диалоге.
+    #: Событие приходит раз в ~5 секунд при наборе текста. $flags = 1.
+    USER_TYPING = 61
+
+    #: Пользователь $user_id набирает текст в беседе $chat_id.
+    USER_TYPING_IN_CHAT = 62
+
+    #: Пользователь $user_id записывает голосовое сообщение в диалоге/беседе $peer_id
+    USER_RECORDING_VOICE = 64
+
+    #: Пользователь $user_id совершил звонок с идентификатором $call_id.
+    USER_CALL = 70
+
+    #: Счетчик в левом меню стал равен $count.
+    MESSAGES_COUNTER_UPDATE = 80
+
+    #: Изменились настройки оповещений.
+    #: $peer_id — идентификатор чата/собеседника,
+    #: $sound — 1/0, включены/выключены звуковые оповещения,
+    #: $disabled_until — выключение оповещений на необходимый срок.
+    NOTIFICATION_SETTINGS_UPDATE = 114
+
 
 class VkPlatform(IntEnum):
     """ Идентификаторы платформ """
@@ -898,10 +991,6 @@ class BotMessageEvent(MessageEvent):
         self.event_id = raw.get("event_id")
         self.v = raw.get("v")
 
-        # ставим type так, как ждёт старый код
-        # если нужно ещё message_edit и т.п. — расширишь mapping
-        self.type = VkEventType.MESSAGE_NEW
-
         self.group_id = raw.get("group_id")
 
         obj = raw.get("object") or {}
@@ -961,7 +1050,7 @@ class BotMessageEvent(MessageEvent):
             if atype and owner_id is not None and media_id is not None:
                 # например: photo12345_67890
                 self.attachments_ids.append(f"{atype}{owner_id}_{media_id}")
-
+        self.type = raw.type
     
 class EventEncoder(json.JSONEncoder):
     def default(self, obj):
